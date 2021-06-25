16d3ead9ebb4655944d2f587e8eeec9701b6b70d
wuchangwei <985215993@qq.com>
2021-06-25 14:59:34 +0800

    uView引入项目初始化完成

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ Vue.config.productionTip = false
 
 App.mpType = 'app'
 
+// 引入全局uView
+import uView from 'uview-ui'
+Vue.use(uView);
+
 const app = new Vue({
     ...App
 })
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,7 +2,15 @@
 	<view class="content">
 		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
-			<text class="title">{{title}}</text>
+			<text class="title">
+				uView - 多平台快速开发的UI框架
+			</text>
+		</view>
+		<view class="button-demo">
+			<u-button :ripple="true">按钮组件演示</u-button>
+		</view>
+		<view class="link-demo">
+			<u-link :color="$u.color['primary']" :under-line="true" href="http://www.uviewui.com">跳转uView文档：www.uviewui.com</u-link>
 		</view>
 	</view>
 </template>
@@ -23,18 +31,19 @@
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
 	.content {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+		padding: 40rpx;
 	}
 
 	.logo {
 		height: 200rpx;
 		width: 200rpx;
-		margin-top: 200rpx;
+		margin-top: 100rpx;
 		margin-left: auto;
 		margin-right: auto;
 		margin-bottom: 50rpx;
@@ -44,9 +53,17 @@
 		display: flex;
 		justify-content: center;
 	}
-
+	
 	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+		font-size: 28rpx;
+		color: $u-content-color;
+	}
+	
+	.button-demo {
+		margin-top: 80rpx;
+	}
+	
+	.link-demo {
+		margin-top: 80rpx;
 	}
 </style>
