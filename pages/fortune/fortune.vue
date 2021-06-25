e46980bfcd63647c1f2d0cad5051b7a8a0daf8c7
wuchangwei <985215993@qq.com>
2021-06-25 15:56:53 +0800

    tab栏完成

---
+++ pages/fortune/fortune.vue
@@ -0,0 +1,23 @@
+<template>
+  <!--财富页面 -->
+  <view>
+    财富
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ pages/loans/loans.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- 祥瑞借 -->
+  <view>
+    祥瑞借
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style>
+
+</style>
